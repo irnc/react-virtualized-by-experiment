96b3d6f35786254e0a67ccbcc0e109e55fd1548f
Pavel Zubkou <pavel.zubkou@gmail.com>
2017-06-21 13:52:24 +0300

    Implement Cell component which focuses itself on activation

--- components/CellRenderer.jsx
+++ components/CellRenderer.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ActiveRenderer from './virtualized/ActiveRenderer';
 
+class Cell extends React.PureComponent {
+  componentDidMount() {
+    if (this.props.active) {
+      this.anchor.focus();
+    }
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.active && prevProps.active !== this.props.active) {
+      this.anchor.focus();
+    }
+  }
+
+  render() {
+    const { active } = this.props;
+
+    return <a href="#" ref={a => (this.anchor = a)}>{this.props.children}</a>;
+  }
+}
+
 export default class CellRenderer extends ActiveRenderer {
   renderer = ({ columnIndex, key, rowIndex, style }) => {
     const borderColor = this.isCellActive(rowIndex, columnIndex) ? 'red' : 'blue';
@@ -15,7 +35,7 @@ export default class CellRenderer extends ActiveRenderer {
           borderColor,
         }}
       >
-        {key}
+        <Cell active={this.isCellActive(rowIndex, columnIndex)}>{key}</Cell>
       </div>
     )
   };
